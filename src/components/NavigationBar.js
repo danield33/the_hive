adb91fe5fc6daed538c816804fedf4de9d975419
Daniel <danieldawit33@gmail.com>
2021-09-04 17:03:30 -0400

    Add navigation bar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import '../index.css'
+
+export function NavigationBar({items}) {
+    return (
+        <div className={'navBar'}>
+
+            {items.map((i,j) => <button key={j}>{i}</button>)}
+
+        </div>
+    );
+};
