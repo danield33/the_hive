86a101435ca2a667036baa8517ac2ef7c642d54e
Daniel <22dawitd@mydacc.org>
2021-09-07 12:25:44 -0400

    Add publish idea button

--- src/components/CreatedList.js
+++ src/components/CreatedList.js
@@ -4,7 +4,9 @@ export function CreatedList({data}) {
     return (
         <div className={'forumList'}>
 
-
+            <div className={'triangle'}>
+                <p style={{transform: 'rotate(310deg)', position: 'absolute', marginLeft: -60, marginTop: 45, textAlign: 'center'}}>Publish Idea</p>
+            </div>
 
         </div>
     );
