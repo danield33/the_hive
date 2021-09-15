90dcc7aff9a7ffbf3fc070bf03b99c44f9840089
CourtneyKlug <koopacat2003@gmail.com>
2021-09-15 14:04:58 -0400

    Add images

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import history from './History.js';
-import {ForumPost, Groups, Account, Home} from "./screens";
+import {ForumPost, Groups, Account, Home, wallpapers} from "./screens";
 
 export default class Routes extends Component {
     render() {
@@ -13,6 +13,7 @@ export default class Routes extends Component {
                     <Route path="/Post" component={ForumPost} />
                     <Route path="/Account" component={Account} />
                     <Route path="/Account" component={Groups} />
+                    <Route path="/Wallpapers" component={wallpapers} />
 
                 </Switch>
             </Router>
