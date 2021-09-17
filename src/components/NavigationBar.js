c2cbe7e9461ca789d2e7f6414614b95129a57032
Daniel <22dawitd@mydacc.org>
2021-09-17 12:59:08 -0400

    Add logo click

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,10 @@ import logo from '../images/logo.svg'
 export function NavigationBar({items}) {
     return (
         <div className={'navBar'}>
-            <img src={logo} alt={""} style={{height: 80, alignSelf: 'center'}}/>
+
+            <Nav.Link href={"/wallpapers"} style={{alignSelf: 'center'}}>
+                <img src={logo} alt={""} style={{height: 80}}/>
+            </Nav.Link>
             {items.map((i, j) => (
                 <Nav.Link href={i.linkTo} className={'navBar'}>
                     <button>
